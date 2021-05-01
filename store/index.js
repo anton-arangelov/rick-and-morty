c9197665cd94da0ea4cf9c0cd22e29fb99398229
anton-arangelov <68649791+anton-arangelov@users.noreply.github.com>
2021-05-01 13:07:20 +0300

    Delete store directory

--- store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import episodesSlice from "./episodes-slice.js";
-
-const store = configureStore({
-  reducer: { episodes: episodesSlice.reducer },
-});
-
-export default store;
